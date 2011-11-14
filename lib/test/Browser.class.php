22de4616cfd1689f41b994c30822b16cbc69eb77
Phoenix Zerin <phoenix@todofixthis.com>
2011-11-14 09:53:40 -0600

    Fixed incorrect output buffering when multiple requests are performed in a single test.

--- lib/test/Browser.class.php
+++ lib/test/Browser.class.php
@@ -213,12 +213,6 @@ class Test_Browser extends Test_ObjectWrapper
     );
     $this->_isCalled = true;
 
-    /* Flush output buffer. */
-    if( ob_get_level() > 0 )
-    {
-      ob_end_clean();
-    }
-
     return $this;
   }
 
